312a603367eb79a132383beb5c3c9267753b7684
Xophu <rick2002rrh@gmail.com>
2019-10-29 10:56:08 +0100

    Full docstring draft

--- functions.py
+++ functions.py
@@ -1,37 +1,43 @@
 import random
 def modeSelection():
+    '''Selects mode'''
     return
 
 
 def checkAnswerOpen():
+    '''Checks answer for open/normal mode'''
     res = ''
     return res
 
 
 def checkAnswerMultipleChoice():
+    '''Checks answer for multiple choice/easy mode'''
     res = ''
     return res
 
 
 def randomHero():
+    '''Selects random hero from dataset'''
     return
 
 
 def apiConversion():
+    '''Converts API data to usable data'''
     return
 
 
 def anonimisation():
-    #Takes hero's name and removes from any hint text. For example replace with [data redacted].
+    '''Takes hero's name and removes from any hint text. For example replace with [data redacted].'''
     return
 
 
 def randomHint():
-    #Takes set of hints, possibly per category, and selects a random hint.
+    '''Takes set of hints, possibly per category, and selects a random hint.'''
     return
 
 
 def points():
-    points = 0
+    '''Starts on 25 and removes 1 for every wrong answer, 3 for every hint. Maybe boolean argument for "hint" or smth if false it must be a wrong answer so you remove 1, if true remove 3'''
+    points = 25
     return points
 
