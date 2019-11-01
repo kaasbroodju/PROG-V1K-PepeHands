93d6eaad6dbdcf17faae20c8e7bd64f6dc61a0fc
Warprince97 <jean_97@live.nl>
2019-11-01 11:25:56 +0100

    jean

--- anonimity.py
+++ anonimity.py
@@ -1,19 +1,19 @@
-### hier wat verschillende opties die getest moeten worden (en moeten worden aangepast naar de correcte objecten ###
+### here are some of the anonimity functions we need to test (and must be changed to fit in with the right objects) ###
 
-charact_dict = get_character()
-charact_dict['desc']['desc'].replace(charact_dict['name'], '[DATA REDACTED]')
-
-
-charact_dict = get_character()
-print('desc'.replace("name", "This character"+', '+'desc'))
-
-#deze werkt denk ik
-    for 'name' in get_character('desc'):
-        print('desc'.replace('name', "This character"))
+#charact_dict = get_character()                              #iteration 4
+#charact_dict['desc']['desc'].replace(charact_dict['name'], '[DATA REDACTED]')
 
 
-    for 'name' in get_character('name'):
-        print('name'.replace('name', "this character"))
+#charact_dict = get_character()                              #iteration 3
+#print('desc'.replace("name", "This character"+', '+'desc'))
 
 
-character_sheet['data']['results'][0]['description'].replace(character_sheet['data']['results'][0]['name'], 'DATA RETRACTED')
+#for 'name' in get_character('desc'):                        #iteration 2
+    #print('desc'.replace('name', "This character"))
+
+
+#for 'name' in get_character('name'):                         #iteration 1
+    #print('name'.replace('name', "this character"))
+
+
+#character_sheet['data']['results'][0]['description'].replace(character_sheet['data']['results'][0]['name'], 'DATA RETRACTED')
